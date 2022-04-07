6bd32fd7e934b74943c27579186c58d52e87d122
Sebastian Ospina <sebastianospinacontacto@gmail.com>
2022-04-07 09:43:37 -0500

    initial commit

---
+++ Consulta.php
@@ -0,0 +1,104 @@
+<?php
+require 'vendor/autoload.php';
+const GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSf1ijgwz_RiQDP9zzI6YeM6n_Hl2Gnad03wu_mimLZ1PxPn-A/viewform';
+
+//Get HTTP params
+$request = parseRequest();
+//Validate the request
+validatedRequest($request);
+
+//Search on DB
+$result = searchUser($request);
+//Format data
+$formattedData = formatData($result);
+//Build final form URL
+
+$finalUrl = buildUrl($formattedData);
+
+//dd($finalUrl);
+header('Location: '.$finalUrl);
+function buildUrl($formattedData)
+{
+    return GOOGLE_FORM_URL . '?' . http_build_query($formattedData);
+}
+
+function validatedRequest($request)
+{
+    $verifiedRequest = verifyRequest($request);
+    //Send error as responses
+
+    if ($verifiedRequest['error'] === true) {
+        response($verifiedRequest, 400);
+    }
+}
+
+function formatData($result)
+{
+    $formItems = getFormItems();
+    $finalResult = [];
+    foreach ($formItems as $key => $formItem) {
+        $finalResult[$formItem['googleFormId']] = $result[$key];
+    }
+    return $finalResult;
+}
+
+function searchUser(Array $request)
+{
+    //TODO: Hacer la petición
+    $endpoint = 'https://academia.unibague.edu.co/atlante/graduados.php';
+    $curl = new \Ospina\CurlCobain\CurlCobain($endpoint);
+    $curl->setQueryParamsAsArray([
+        'consulta' => 'Consultar',
+        'documento' => $request['identification_number'],
+        'dia' => $request['day'],
+        'mes' => $request['month']
+    ]);
+    $response = $curl->makeRequest();
+
+    return json_decode($response, true)[0];
+}
+
+ function getFormItems()
+{
+    return [
+        "Nombres" => ["answer" => null, "googleFormId" => "entry.98280260"],
+        "Apellidos" => ["answer" => null, "googleFormId" => "entry.275477632"],
+        "Numero de identificacion" => ["answer" => null, "googleFormId" => "entry.315622645"],
+        "Telefono alterno" => ["answer" => null, "googleFormId" => "entry.60355407"],
+        "Telefono de contacto" => ["answer" => null, "googleFormId" => "entry.42357451"],
+        "Annio de graduacion" => ["answer" => null, "googleFormId" => "entry.1060955643"],
+        "Direccion de correspondencia" => ["answer" => null, "googleFormId" => "entry.1600532275"],
+        "Programa del cual es egresado" => ["answer" => null, "googleFormId" => "entry.666708053"],
+        "Nivel académico alcanzado" => ["answer" => null, "googleFormId" => "entry.865564300"],
+        "Correo" => ["answer" => null, "googleFormId" => "emailAddress"],
+    ];
+}
+
+function response($content, $status)
+{
+    header('Content-Type: application/json');
+    http_response_code($status);
+    echo json_encode($content);
+    die();
+}
+
+function parseRequest()
+{
+    return $_REQUEST;
+}
+
+
+function verifyRequest(array $request)
+{
+    if (empty($request['identification_number']) || empty($request['day']) || empty($request['month']) ) {
+        return ['error' => true, 'msg' => 'Por favor ingresa tu número de documento y fecha de nacimiento'];
+    }
+    return ['error' => false];
+
+}
+
+function dd($var)
+{
+    print_r($var);
+    die();
+}
